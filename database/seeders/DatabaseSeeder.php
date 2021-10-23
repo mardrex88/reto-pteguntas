63d0e48918a5f33f29f6bdd87f718444a3fdd15c
Mardrex <mardrex88@gmail.com>
2021-10-23 14:57:46 -0500

    creamos un Seed para poder ingresar 5 categorias en la base de datos de manera inicil

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,8 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
 
+use App\Models\Categoria;
+
 class DatabaseSeeder extends Seeder
 {
     /**
@@ -13,6 +15,34 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        $categoria = new Categoria();
+        $categoria->nombre = "Facil";
+        $categoria->nivel = 1;
+        $categoria->puntos = 10;
+        $categoria->save();
+
+        $categoria2 = new Categoria();
+        $categoria2->nombre = "Normal";
+        $categoria2->nivel = 2;
+        $categoria2->puntos = 20;
+        $categoria2->save();
+
+        $categoria3 = new Categoria();
+        $categoria3->nombre = "Dificil";
+        $categoria3->nivel = 3;
+        $categoria3->puntos = 30;
+        $categoria3->save();
+
+        $categoria = new Categoria();
+        $categoria4->nombre = "Experto";
+        $categoria4->nivel = 4;
+        $categoria4->puntos = 40;
+        $categoria4->save();
+
+        $categoria5 = new Categoria();
+        $categoria5->nombre = "Leyenda";
+        $categoria5->nivel = 5;
+        $categoria5->puntos = 50;
+        $categoria5->save();
     }
 }
